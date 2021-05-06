e12b55058060c0af43081e47d9506a9b14d9dc9e
dulee19 <adusan53@gmail.com>
2021-05-06 21:05:24 +0200

    native

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, FlatList } from "react-native";
 import Header from "./components/Header";
+import TodoItem from "./components/todoItem";
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -8,6 +9,12 @@ export default function App() {
     { text: "learn norwegian", key: "2" },
     { text: "learn react native", key: "3" }
   ]);
+
+  const pressHandler = key => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.key != key);
+    });
+  };
 
   return (
     <View style={styles.container}>
@@ -17,7 +24,9 @@ export default function App() {
         <View style={styles.list}>
           <FlatList
             data={todos}
-            renderItem={({ item }) => <Text>{item.text}</Text>}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
           />
         </View>
       </View>
